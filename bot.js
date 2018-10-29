d7b191d76674044a35c0c906727bf944ae79a0aa
Moody0 <44557769+Moody0@users.noreply.github.com>
2018-10-29 02:44:18 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('ready', async() => {
+var server = "499671857490952205"; // ايدي السررفر
+var channel = "505108060105211937";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
+    },6);
+})
+
+client.on('ready', async() => {
+var server = "499671857490952205"; // ايدي السررفر
+var channel = "505108060105211937";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('سى سى سى سى سى سى سى سى سى سى سى سى سى سى سى سى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىر سى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىسى سى سىرسى سى سى')
+    },5);
+})
+
+client.on('ready', async() => {
+var server = "505108060105211928"; // ايدي السررفر
+var channel = "505108060105211937";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج كل زج')
+    },4);
+})
+
+client.on('ready', async() => {
+var server = "505108060105211928"; // ايدي السررفر
+var channel = "505108060105211937";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('شو شو شو شو شو شو شو شو شو شو شو شو شو شو شو شو شو شو شو شو شو ر ر شو شو شو شو شو ر شو شو شو شو شو')
+    },3);
+})
+
+client.on('ready', async() => {
+var server = "505108060105211928"; // ايدي السررفر
+var channel = "505108060105211937";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلى ريلىر ريلى ريلى ريلىريلى ريلى ريلى ريلى ريلى ريلى')
+    },2);
+})
+
+client.on('ready', async() => {
+var server = "505108060105211928"; // ايدي السررفر
+var channel = "505108060105211937";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('فاك فاك فاك فاك فاك فاك فاك فاك فاك رفاك فاك فاك فاك فاك فاك فاك فاك فاك فاك فاك فاك فاك فاكفاك فاك فاك فاك فاك فاك فاك فاك فاك فاك فاكفاكفاك فاك فاك')
+    },1);
+})
+
+
+client.login("NTAzMjM2NTM4MjQzNDgxNjAx.Dqzj1A.u2h9SoIXUxNi-duQN-teOHYXq-E");
